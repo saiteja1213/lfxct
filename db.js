d520c822cbba90a6233bf0ce039d2c9141b0a036
saiteja1213 <69451084+saiteja1213@users.noreply.github.com>
2026-03-11 20:25:04 -0400

    Create db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+const sql = require("mssql");
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    options: { encrypt: true }
+};
+
+module.exports = { sql, config };
